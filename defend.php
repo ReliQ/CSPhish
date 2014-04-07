c452ee03d0ed0d70c33960d142a08b313fe29db5
ReliQ <versatile.jamrok@gmail.com>
2014-04-07 17:51:49 -0500

    ...

---
+++ defend.php
@@ -0,0 +1,76 @@
+<?php	
+/**
+ *	CSPhish
+ *	A Computer Security assignment.
+ *
+ * 	@author Patrick Reid
+ * 	@link http://www.reliqartz.com
+ */
+?>
+<?php require_once("./incl/header.php"); ?>
+
+<section id="in">
+	<div class="intro">
+		<h1>Ways to Defend Against Phishing</h1>
+	</div>
+	<div class="actions">
+		<a class="act" href= "./">Return to main</a>
+		<a class="doc-dl" href="./files/Phishing.docx">Download <img src="./files/word2013.png" alt="Download" /></a>
+		<ol>
+			<li>
+				<h3>Keep your operating system patched to avoid known software vulnerabilities from being exploited.</h3>
+				<p>
+					Install patches from software manufacturers as soon as they are distributed, since hackers can quickly assemble malware using pre-made components to exploit the vulnerability before the majority of people download the fix. A fully patched computer behind a firewall is the best defense against Trojan and spyware installation.
+				</p></li>
+			<li>
+				<h3>Download the latest version of your browser to ensure that it is also fully updated and utilizes the latest technologies.</h3>
+				<p>
+					Internet Explorer 7 and other browsers include an anti-phishing toolbar to add another layer of protection.
+				</p></li>
+			<li>
+				<h3>Check the domain name of the site as an indicator of whether the site is legitimate. </h3>
+				<p>
+					The origin of an email, the location of a page, and the use of SSL encryption can all be spoofed. Browser lock icons can also be spoofed. You should ensure SSL is being used (look for "https:" in the URL). Because of hacker tricks, though, you can't rely on these checks as an absolute indicator that the communication or site is safe.
+				</p></li>
+			<li>
+				<h3>Never click on links in an unsolicited email, and ignore call-to-action emails such as "Your account will be terminated."</h3>
+				<p>
+					Call the company on the phone instead, using a phone number that you verify outside of the email.
+				</p></li>
+			<li>
+				<h3>Be very careful when downloading any software from the web.</h3>
+				<p>
+					Spyware can piggyback onto legitimate software, or the software may contain key loggers or screen scrapers that steal your information. You should completely avoid free screen savers and other freebies. Also be wary of opening an email attachment—a video, graphic, or PDF—even from someone you know. Virus-scanning software protects you by determining if viruses are hiding inside before you open the attachment.
+				</p></li>
+			<li>
+				<h3>Use software that automatically checks to see whether a URL is safe before you are taken to the site.</h3>
+				<p>
+					Check out McAfee SiteAdvisor - a free online safety tool that provides ratings on over 95% of all web sites on the Internet. You can also check the validity of individual web addresses (URLs) with a WHOIS search such as www.DNSstuff.com, which has a search tool that displays the contact information for a domain/IP based in almost any country.
+				</p></li>
+			<li>
+				<h3>Use an Internet service provider (ISP) that implements strong anti-spam and anti-phishing technologies and policies. </h3>
+				<p>
+					For example, AOL blocks known phishing sites so that customers can't reach them. The SpamHaus organization (www.spamhaus.org) lists the current top-10 worst ISPs in this category—consider this when making your choice.
+				</p></li>
+			<li>
+				<h3>Review credit card and bank account statements as soon as you receive them to check for unauthorized charges.</h3>
+				<p>
+					If your statement is late by more than a couple of days, call your credit card company or bank to confirm your billing address and account balances.
+				</p></li>
+			<li>
+				<h3>Be an early adopter of new technologies. </h3>
+				<p>
+					New validation techniques are being used by banks and credit card companies to make online transactions more secure, so make sure to take advantage of them. The computer industry is also working on authentication technologies such as Sender ID, Domain Name, and S/MIME, which will greatly reduce the effectiveness of phishing attacks.
+				</p></li>
+			<li>
+				<h3>Protect your computer with strong security software and make sure to keep it up to date.</h3>
+				<p>
+					Hackers have databases containing millions of email addresses. They target vulnerabilities in email applications and web browsers, and abuse design vulnerabilities in targeted web site programs. You can defend against phishing, though, because it blends existing techniques of spam and software exploitation. 
+McAfee® Internet Security Suite guarantees trusted PC protection from viruses, hackers, and spyware. Its cutting-edge features include X-Ray for Windows®, which detects and kills rootkits and other malicious applications that hide from Windows and other anti-virus programs. Its integrated anti-virus, anti-spyware, firewall, anti-spam, anti-phishing, and backup technologies work together to combat today's sophisticated, blended attacks.
+				</p></li>
+		</ol>
+		<a class="act" href= "./">Return to main</a>
+	</div>
+</section>
+	
+<?php require_once("./incl/footer.php"); ?>
